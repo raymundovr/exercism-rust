a4e5562d97132da790815215d3c5514f86dc2c40
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2019-12-19 20:24:45 +0100

    completed hig-scores

---
+++ high-scores/src/lib.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.iter().last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut sorted = self.scores.to_vec();
+        sorted.sort();
+        sorted.iter().rev().take(3).copied().collect::<Vec<_>>()
+    }
+}
